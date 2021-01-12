b391d2d7abbda293490dc1bf77e1bb98eec1064f
ThiagoAZ <thiagoaugustozeferino@gmail.com>
2021-01-11 21:14:07 -0300

    Primeira Versão Do Exercicio de Mudar Imagens

---
+++ Aulas_HTML/Exercicio_troca_de_imagem/funcionalidade.js
@@ -0,0 +1,183 @@
+let clarice_linspector = document.querySelector('#clarice_linspector')
+let principal_clarice_linspector = document.querySelector('#principal_clarice_linspector')
+
+let jorge_amado = document.querySelector('#jorge_amado')
+let principal_jorge_amado = document.querySelector('#principal_jorge_amado')
+
+let machado_de_assis = document.querySelector('#machado_de_assis')
+let principal_machado_de_assis = document.querySelector('#principal_machado_de_assis')
+
+let jose_de_alencar = document.querySelector('#jose_de_alencar')
+let principal_jose_de_alencar = document.querySelector('#principal_jose_de_alencar')
+
+let luiz_gama = document.querySelector('#luiz_gama')
+let principal_luiz_gama = document.querySelector('#principal_luiz_gama')
+
+
+clarice_linspector.addEventListener('click', function() {
+
+    if (principal_clarice_linspector.style.opacity === '0' ) {
+
+        principal_clarice_linspector.style.opacity = '1'
+
+    }
+
+    if (principal_jorge_amado.style.opacity === '1' ) {
+
+        principal_jorge_amado.style.opacity = '0'
+
+    }
+    if (principal_machado_de_assis.style.opacity === '1' ) {
+
+        principal_machado_de_assis.style.opacity = '0'
+
+    }
+    if (principal_jose_de_alencar.style.opacity === '1' ) {
+
+        principal_jose_de_alencar.style.opacity = '0'
+
+    }
+    if (principal_luiz_gama.style.opacity === '1' ) {
+
+        principal_luiz_gama.style.opacity = '0'
+
+    }
+
+
+
+    });
+
+    
+jorge_amado.addEventListener('click', function() {
+
+    if (principal_clarice_linspector.style.opacity === '1' ) {
+
+        principal_clarice_linspector.style.opacity = '0'
+
+    }
+
+    if (principal_jorge_amado.style.opacity === '0' ) {
+
+        principal_jorge_amado.style.opacity = '1'
+
+    }
+    if (principal_machado_de_assis.style.opacity === '1' ) {
+
+        principal_machado_de_assis.style.opacity = '0'
+
+    }
+    if (principal_jose_de_alencar.style.opacity === '1' ) {
+
+        principal_jose_de_alencar.style.opacity = '0'
+
+    }
+    if (principal_luiz_gama.style.opacity === '1' ) {
+
+        principal_luiz_gama.style.opacity = '0'
+
+    }
+
+
+
+    });
+
+
+machado_de_assis.addEventListener('click', function() {
+
+    if (principal_clarice_linspector.style.opacity === '1' ) {
+
+        principal_clarice_linspector.style.opacity = '0'
+
+    }
+
+    if (principal_jorge_amado.style.opacity === '1' ) {
+
+        principal_jorge_amado.style.opacity = '0'
+
+    }
+    if (principal_machado_de_assis.style.opacity === '0' ) {
+
+        principal_machado_de_assis.style.opacity = '1'
+
+    }
+    if (principal_jose_de_alencar.style.opacity === '1' ) {
+
+        principal_jose_de_alencar.style.opacity = '0'
+
+    }
+    if (principal_luiz_gama.style.opacity === '1' ) {
+
+        principal_luiz_gama.style.opacity = '0'
+
+    }
+
+
+
+    });
+
+
+jose_de_alencar.addEventListener('click', function() {
+
+    if (principal_clarice_linspector.style.opacity === '1' ) {
+
+        principal_clarice_linspector.style.opacity = '0'
+
+    }
+
+    if (principal_jorge_amado.style.opacity === '1' ) {
+
+        principal_jorge_amado.style.opacity = '0'
+
+    }
+    if (principal_machado_de_assis.style.opacity === '1' ) {
+
+        principal_machado_de_assis.style.opacity = '0'
+
+    }
+    if (principal_jose_de_alencar.style.opacity === '0' ) {
+
+        principal_jose_de_alencar.style.opacity = '1'
+
+    }
+    if (principal_luiz_gama.style.opacity === '1' ) {
+
+        principal_luiz_gama.style.opacity = '0'
+
+    }
+
+
+
+    });
+
+luiz_gama.addEventListener('click', function() {
+
+    if (principal_clarice_linspector.style.opacity === '1' ) {
+
+        principal_clarice_linspector.style.opacity = '0'
+
+    }
+
+    if (principal_jorge_amado.style.opacity === '1' ) {
+
+        principal_jorge_amado.style.opacity = '0'
+
+    }
+    if (principal_machado_de_assis.style.opacity === '1' ) {
+
+        principal_machado_de_assis.style.opacity = '0'
+
+    }
+    if (principal_jose_de_alencar.style.opacity === '1' ) {
+
+        principal_jose_de_alencar.style.opacity = '0'
+
+    }
+    if (principal_luiz_gama.style.opacity === '0' ) {
+
+        principal_luiz_gama.style.opacity = '1'
+
+    }
+
+
+
+    });
